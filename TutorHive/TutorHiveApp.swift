a0f2c87b50131997ed9394f0aa7f436669013472
Rodrigo Chávez <rodrichavezm@gmail.com>
2023-03-15 16:10:08 -0400

    Initial Commit

---
+++ TutorHive/TutorHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorHiveApp.swift
+//  TutorHive
+//
+//  Created by Rodrigo Chavez on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct TutorHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
