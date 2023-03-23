e81795f6a6580a842b0833f68710368453ad7655
Moanisha <moanisha94@gmail.com>
2023-03-22 21:19:03 -0400

    added contact and profile screens

---
+++ TutorHive/ContactView.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+struct ContactView: View {
+    let skills = ["Swift", "Python", "Java", "JavaScript"]
+    let languages = ["English", "Spanish", "French", "German"]
+
+    @State var title: String = ""
+    @State var selectedSkill: String = ""
+    @State var selectedLanguage: String = ""
+    @State var description: String = ""
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Image("wise1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+
+
+            Text("TutorHive")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+
+            VStack(spacing: 20) {
+                TextField("Title", text: $title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                DropdownTextField(
+                    placeholder: "Skills",
+                    options: skills,
+                    selection: $selectedSkill
+                )
+
+                DropdownTextField(
+                    placeholder: "Languages",
+                    options: languages,
+                    selection: $selectedLanguage
+                )
+
+                TextEditor(text: $description)
+                    .frame(maxHeight: 100)
+                    .border(Color.gray, width: 0.5)
+                    .overlay(
+                            Group {
+                                if description.isEmpty {
+                                    Text("Description")
+                                        .foregroundColor(Color.gray)
+                                        .padding(.horizontal, 4)
+                                        .padding(.vertical, 8)
+                                }
+                            }, alignment: .topLeading)
+            }
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            Button("Submit") {
+                // Handle submission here
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.black)
+            .cornerRadius(10)
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 30)
+        }
+    }
+}
+
+struct ContactView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactView()
+    }
+}
---
+++ TutorHive/ProfileView.swift
@@ -0,0 +1,97 @@
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var dob = Date()
+    @State private var email = ""
+    @State private var phoneNumber = ""
+    @State var selectedGender: String = ""
+let gender = ["male", "female"]
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("PROFILE")
+                .font(.title)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .top)
+                .padding(50)
+
+            TextField("First Name", text: $firstName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            TextField("Last Name", text: $lastName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            DatePicker("Date of Birth", selection: $dob, displayedComponents: [.date])
+                .datePickerStyle(CompactDatePickerStyle())
+
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            TextField("Phone Number", text: $phoneNumber)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            DropdownTextField(
+                               placeholder: "Gender",
+                               options: gender,
+                               selection: $selectedGender
+                           )
+            Spacer()
+
+            Button(action: saveProfile) {
+                Text("Save")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .background(Color.black)
+                    .cornerRadius(5)
+            }
+            .frame(maxWidth: .infinity, alignment: .bottom)
+        }
+        .padding()
+    }
+
+    func saveProfile() {
+        // Implement saving of profile data here
+    }
+}
+struct DropdownTextField: View {
+    let placeholder: String
+    let options: [String]
+    @Binding var selection: String
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color.gray, lineWidth: 0.5)
+            Menu {
+                ForEach(options, id: \.self) { option in
+                    Button(option) {
+                        self.selection = option
+                    }
+                }
+            } label: {
+                VStack(spacing: 20){
+                    HStack{
+                        Text(selection.isEmpty ? placeholder : selection)
+                            .foregroundColor(selection.isEmpty ? .gray : .black)
+                        Spacer()
+                        Image(systemName: "chevron.down")
+                            .foregroundColor(Color.black)
+                            .font(Font.system(size: 10))
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .menuStyle(BorderlessButtonMenuStyle())
+            .frame(maxWidth: .infinity)
+        }
+        .frame(width: .infinity, height: 40)
+    }
+}
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
