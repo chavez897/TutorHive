45f6885915bf09e52b36f0121afb2a6429d439f5
Darshit9428 <darshitpatel9428@gmail.com>
2023-04-19 16:22:13 -0400

    Statistics hard coded

--- TutorHive/Statistics.swift
+++ TutorHive/Statistics.swift
@@ -6,9 +6,47 @@
 //
 
 import SwiftUI
+import FirebaseStorage
+import FirebaseFirestore
+
+class CountViewModel: ObservableObject {
+    
+    private let db = Firestore.firestore()
+    
+    @Published var count = 0
+    @State private var title = ""
+    @State private var skill = ""
+    
+    func calculateCount() {
+        let collectionRef = db.collection("contacts")
+        let valueToQuery = "UApFAQO1Poyy0g5bJm6f"
+        collectionRef.whereField("tutor", isEqualTo: valueToQuery).getDocuments() { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self.count = querySnapshot?.documents.count ?? 0
+            }
+        }
+        
+    }
+    
+        
+    
+}
 
 struct Statistics: View {
+    
+    @StateObject var viewModel = CountViewModel()
+    
+    //@State var documentData: [String: Any] = [:]
+    @State var title: String = ""
+    @State var lastname: String = ""
+    @State var skills: [String] = []
+    private let db = Firestore.firestore()
+    
     var body: some View {
+        
+        
         VStack(alignment:.center,spacing:30) {
             HStack {
                 Spacer()
@@ -28,14 +66,27 @@ struct Statistics: View {
             
             HStack{
                 VStack{
-                    Text("Andre Ainsley")
+                    Text("\(title) \(lastname)")
                         .font(.system(size: 40))
                         .foregroundColor(Color(red: 62/255, green: 78/255, blue: 51/255))
                         .offset(y:-10)
                     
                     
                 }
+            }.onAppear {
+                let documentRef = db.collection("tutors").document("UApFAQO1Poyy0g5bJm6f")
+                documentRef.getDocument() { (documentSnapshot, error) in
+                    if let error = error {
+                        print("Error getting document: \(error)")
+                    } else if let document = documentSnapshot?.data() {
+                            title=document["name"] as? String ?? ""
+                            lastname=document["lastName"] as? String ?? ""
+                    } else {
+                        print("Document does not exist")
+                    }
+                }
             }
+        
             HStack{
                 Text("Statistics / week")
                     .font(.system(size: 20))
@@ -46,11 +97,12 @@ struct Statistics: View {
                 Text("Contacted no of times: ")
                     .font(.system(size: 20))
                     .foregroundColor(Color(red: 62/255, green: 78/255, blue: 51/255))
-                    
-                Text("8")
+                Text("\(viewModel.count) times")
                     .font(.system(size: 40))
                     .foregroundColor(Color(red: 62/255, green: 78/255, blue: 51/255))
                   
+            }.onAppear {
+                viewModel.calculateCount()
             }
             HStack{
                 Text("Skills")
@@ -60,12 +112,25 @@ struct Statistics: View {
             }
             HStack{
                 List {
-                                Text("React")
-                                Text("Angular")
-                                Text("Java")
+                                ForEach(skills, id: \.self) { item in
+                                    Text(item)
+                                .padding()
+                                }
+                
                             }
                             .listStyle(InsetGroupedListStyle())
                     
+            }.onAppear {
+                let documentRef = db.collection("tutors").document("UApFAQO1Poyy0g5bJm6f")
+                documentRef.getDocument() { (documentSnapshot, error) in
+                    if let error = error {
+                        print("Error getting document: \(error)")
+                    } else if let document = documentSnapshot?.data(),let skill = document["skills"] as? [String] {
+                        self.skills = skill
+                    }  else {
+                        print("Document does not exist")
+                    }
+                }
             }
         }
     }
